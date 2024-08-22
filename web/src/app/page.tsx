7e6bda11cdc445662fa4827c10096590d26f1c79
xiter22222 <pawanpediredla@gmail.com>
2024-08-22 11:56:37 +0530

    :0

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import LandingPage from "../../marketing/landing";
+import LandingPage from "../../../marketing/landing";
 
 export default function Home() {
   return (
